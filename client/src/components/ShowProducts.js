b6cdfe22d2faf8f8984705c3f7f10f413d4b4c80
tmachine <tmachine@sys08.PEC.IN>
2024-01-19 11:57:25 +0530

    getting response test using axios

--- client/src/components/ShowProducts.js
+++ client/src/components/ShowProducts.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function ShowProducts() {
+
+  const[products,setProducts]=useState([]);
+
+  const getProducts=async()=>{
+
+    const response=await axios.get('http://localhost:8000/api/')
+    console.log(response.data)
+
+  }
+
+  useEffect(()=>{
+
+    getProducts()
+
+  },[])
+
   return (
     <h1>Show all products</h1>
   )
