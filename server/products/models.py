ec797aa76c9e3a4f2d704325b8228a05c66fccb9
tmachine <tmachine@sys08.PEC.IN>
2024-01-19 09:31:53 +0530

    api

--- server/products/models.py
+++ server/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class product(models.Model):
+    name=models.CharField(max_length=30,null=False,blank=False)
+    desc=models.TextField()
+    price=models.IntegerField(null=False,blank=False)
+    category=models.CharField(max_length=20,null=False,blank=False)
+    image=models.ImageField(upload_to='uploads/images',null=False,blank=False)
+
+    def __str__(self):
+        return self.name
--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import product
+from .serializers import productSerializers
 
-# Create your views here.
+class ProductView(viewsets.ModelViewSet):
+    queryset=product.objects.all()
+    serializer_class=productSerializers
