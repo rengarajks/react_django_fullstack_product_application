549fbffb9d5fef11169c000804e2db285dfe05a4
tmachine <tmachine@sys08.PEC.IN>
2024-01-19 10:56:21 +0530

    nav bar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import ShowProducts from './components/ShowProducts';
 import AddProduct from './components/AddProduct';
 import {Routes,Route,BrowserRouter} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-
-      
        <BrowserRouter>
+       <NavBar/>
        <Routes>
+
+
           <Route  path='/' element={<ShowProducts/>}></Route>
           
           <Route path='/add' element={<AddProduct/>}></Route>
