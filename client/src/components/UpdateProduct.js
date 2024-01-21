8a2d9370daac8b7c66816ff17572b5baaae06e9d
unknown <rengaraj02k@gmail.com>
2024-01-21 21:36:58 +0530

    post updated

--- client/src/components/UpdateProduct.js
+++ client/src/components/UpdateProduct.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { useParams,useNavigate } from 'react-router-dom';
 
 const UpdateProduct = () => {
+  const[name,setName]=useState("");
+  const[desc,setDesc]=useState("");
+  const[price,setPrice]=useState("");
+  const[category,setCategory]=useState("");
+  const[image,setImage]=useState(null);
+
+  const history=useNavigate();
+
+  const {id}=useParams();
+  const loadProducts=async()=>{
+
+    const {data}=await axios.get(`http://127.0.0.1:8000/api/${id}/`)
+    console.log(data)
+  
+
+
+
+  }
+
+  useEffect(()=>{
+     loadProducts()
+  },[])
+
+
   return (
-    <div>UpdateProduct</div>
+    <div>{name}</div>
+
   )
 }
 
